e30b852bafe45177a674c4585e3fab01f797b980
Himanshu <zhimanshuk135@gmail.com>
2022-06-14 23:34:09 +0530

    Added some styles to the UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.css"
 function Navbar({AddBox}) {
   return (
     <div className={styles.Navbar}>
-        <h1>Tranlate_It!</h1>
+        <h1 className={styles.title}>Tranlate_It!</h1>
         <button className={styles.button} onClick={()=>AddBox()}>New language</button>
     </div>
   )
